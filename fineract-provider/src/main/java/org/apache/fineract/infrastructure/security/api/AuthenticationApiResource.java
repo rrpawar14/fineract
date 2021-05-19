fafb5b00f553aa889bfb1111bea95f9591da79ed
rrpawar96 <rrpawar141996@gmail.com>
2021-05-19 14:19:19 +0530

    authentication-based-roles

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/api/AuthenticationApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/security/api/AuthenticationApiResource.java
@@ -26,18 +26,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.fineract.infrastructure.core.data.EnumOptionData;
 import org.apache.fineract.infrastructure.core.serialization.ToApiJsonSerializer;
+import org.apache.fineract.infrastructure.security.constants.TwoFactorConstants;
 import org.apache.fineract.infrastructure.security.data.AuthenticatedUserData;
 import org.apache.fineract.infrastructure.security.service.SpringSecurityPlatformSecurityContext;
 import org.apache.fineract.infrastructure.security.service.TwoFactorUtils;
+import org.apache.fineract.useradministration.data.RoleData;
 import org.apache.fineract.useradministration.domain.AppUser;
+import org.apache.fineract.useradministration.domain.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
@@ -128,29 +134,34 @@ public class AuthenticationApiResource {
             final byte[] base64EncodedAuthenticationKey = Base64.encode(request.username + ":" + request.password);
 
             final AppUser principal = (AppUser) authenticationCheck.getPrincipal();
-            /*
-             * final Collection<RoleData> roles = new ArrayList<>(); final Set<Role> userRoles = principal.getRoles();
-             * for (final Role role : userRoles) { roles.add(role.toData()); }
-             *
-             * final Long officeId = principal.getOffice().getId(); final String officeName =
-             * principal.getOffice().getName();
-             *
-             * final Long staffId = principal.getStaffId(); final String staffDisplayName =
-             * principal.getStaffDisplayName();
-             *
-             * final EnumOptionData organisationalRole = principal.organisationalRoleData();
-             */
-            // boolean isTwoFactorRequired = twoFactorUtils.isTwoFactorAuthEnabled() &&
-            // !principal.hasSpecificPermissionTo(TwoFactorConstants.BYPASS_TWO_FACTOR_PERMISSION);
-            /*
-             * if (this.springSecurityPlatformSecurityContext.doesPasswordHasToBeRenewed(principal)) {
-             * authenticatedUserData = new AuthenticatedUserData(request.username, principal.getId(), new
-             * String(base64EncodedAuthenticationKey, StandardCharsets.UTF_8), isTwoFactorRequired); } else {
-             *
-             * authenticatedUserData = new AuthenticatedUserData(request.username, officeId, officeName, staffId,
-             * staffDisplayName, organisationalRole, roles, permissions, principal.getId(), new
-             * String(base64EncodedAuthenticationKey, StandardCharsets.UTF_8), isTwoFactorRequired); }
-             */
+
+            final Collection<RoleData> roles = new ArrayList<>();
+            final Set<Role> userRoles = principal.getRoles();
+            for (final Role role : userRoles) {
+                roles.add(role.toData());
+            }
+
+            final Long officeId = principal.getOffice().getId();
+            final String officeName = principal.getOffice().getName();
+
+            final Long staffId = principal.getStaffId();
+            final String staffDisplayName = principal.getStaffDisplayName();
+
+            final EnumOptionData organisationalRole = principal.organisationalRoleData();
+
+            boolean isTwoFactorRequired = twoFactorUtils.isTwoFactorAuthEnabled()
+                    && !principal.hasSpecificPermissionTo(TwoFactorConstants.BYPASS_TWO_FACTOR_PERMISSION);
+
+            if (this.springSecurityPlatformSecurityContext.doesPasswordHasToBeRenewed(principal)) {
+                authenticatedUserData = new AuthenticatedUserData(request.username, principal.getId(),
+                        new String(base64EncodedAuthenticationKey, StandardCharsets.UTF_8), isTwoFactorRequired);
+            } else {
+
+                authenticatedUserData = new AuthenticatedUserData(request.username, officeId, officeName, staffId, staffDisplayName,
+                        organisationalRole, roles, permissions, principal.getId(),
+                        new String(base64EncodedAuthenticationKey, StandardCharsets.UTF_8), isTwoFactorRequired);
+            }
+
         }
 
         return this.apiJsonSerializerService.serialize(authenticatedUserData);
